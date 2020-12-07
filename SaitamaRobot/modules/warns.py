629b2018e33818b116408d5c44159bf0ae5cb3a1
Rihad <74430658+Mensimov@users.noreply.github.com>
2020-12-07 15:17:54 +0400

    Update warns.py

--- SaitamaRobot/modules/warns.py
+++ SaitamaRobot/modules/warns.py
@@ -143,7 +143,7 @@ def button(update: Update, context: CallbackContext) -> str:
         res = sql.remove_warn(user_id, chat.id)
         if res:
             update.effective_message.edit_text(
-                "Warn removed by {}.".format(
+                "Xəbərdarlıq {} tərəfindən silindi.".format(
                     mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML)
             user_member = chat.get_member(user_id)
