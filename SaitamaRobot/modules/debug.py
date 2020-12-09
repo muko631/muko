da2acc7349caa0c5f47e7fb845eafca5ced27935
Rihad <74430658+Mensimov@users.noreply.github.com>
2020-12-09 13:24:12 +0400

    Update debug.py

--- SaitamaRobot/modules/debug.py
+++ SaitamaRobot/modules/debug.py
@@ -21,15 +21,15 @@ def debug(update: Update, context: CallbackContext):
     if len(args) > 1:
         if args[1] in ('yes', 'on'):
             DEBUG_MODE = True
-            message.reply_text("Debug mode is now on.")
+            message.reply_text("Debug modu aktiv edildi.")
         elif args[1] in ('no', 'off'):
             DEBUG_MODE = False
-            message.reply_text("Debug mode is now off.")
+            message.reply_text("Debug modu deaktiv edildi.")
     else:
         if DEBUG_MODE:
-            message.reply_text("Debug mode is currently on.")
+            message.reply_text("Debug modu hazırda aktivdir.")
         else:
-            message.reply_text("Debug mode is currently off.")
+            message.reply_text("Debug modu hazırda deaktivdir.")
 
 
 @telethn.on(events.NewMessage(pattern="[/!].*"))
