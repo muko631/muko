32a171026700194de44649d6c61aa38b0f499210
Rihad <74430658+Mensimov@users.noreply.github.com>
2020-12-09 19:34:42 +0400

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -49,7 +49,7 @@ def cb_sticker(update: Update, context: CallbackContext):
     results = soup.find_all("a", {'class': "sticker-pack__btn"})
     titles = soup.find_all("div", "sticker-pack__title")
     if not results:
-        msg.reply_text('No results found :(.')
+        msg.reply_text('Heç bir nəticə tapılmadı :(.')
         return
     reply = f"*{split[1]}* stiker axtarışının nəticələri:"
     for result, title in zip(results, titles):
