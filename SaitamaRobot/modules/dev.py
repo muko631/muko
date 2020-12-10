1268f432827e55647ac29120fb5f36c8ac5d1d2c
Rihad <74430658+Mensimov@users.noreply.github.com>
2020-12-10 10:45:48 +0400

    Update dev.py

--- SaitamaRobot/modules/dev.py
+++ SaitamaRobot/modules/dev.py
@@ -18,28 +18,28 @@ def leave(update: Update, context: CallbackContext):
         chat_id = str(args[0])
         try:
             bot.leave_chat(int(chat_id))
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("Qrupu tərk etdim!.")
         except TelegramError:
             update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho).")
+                "Əməliyyat uğursuz oldu.")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
+        update.effective_message.reply_text("Keçərli qrup ID ver")
 
 
 @run_async
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
     sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart.")
+        "Dəyişiklər əldə edilir.")
     subprocess.Popen('git pull', stdout=subprocess.PIPE, shell=True)
 
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+    sent_msg_text = sent_msg.text + "\n\nDəyişiklər əldə edildi...Yenidən başladılır. "
 
     for i in reversed(range(5)):
         sent_msg.edit_text(sent_msg_text + str(i + 1))
         sleep(1)
 
-    sent_msg.edit_text("Restarted.")
+    sent_msg.edit_text("Yenidən başladıldı.")
 
     os.system('restart.bat')
     os.execv('start.bat', sys.argv)
@@ -49,7 +49,7 @@ def gitpull(update: Update, context: CallbackContext):
 @dev_plus
 def restart(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one")
+        "Yenidən başladılır")
 
     os.system('restart.bat')
     os.execv('start.bat', sys.argv)
